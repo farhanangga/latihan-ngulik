c5d432bc3f26a7294aff658e3c62ac088790356c
Farhan Angga <farhanangga8900@gmail.com>
2026-01-05 23:22:35 +0700

    finish latihan-1

--- app/latihan-1/mine/page.tsx
+++ app/latihan-1/mine/page.tsx
@@ -1,17 +1,26 @@
 export default async function mine(){
 
     const button = [
-        {id:1 , name: "Button 1", color: "bg-blue-400"},
-        {id:2 , name: "Button 2", color: "bg-red-400"},
-        {id:3 , name: "Button 3", color: "bg-green-400"},
-    ]
+        {id:1 , name: "Button 1"},
+        {id:2 , name: "Button 2"},
+        {id:3 , name: "Button 3"},
+        {id:4 , name: "Button 4"},
+        {id:5 , name: "Button 5"},
+        {id:6 , name: "Button 6"},
+    ]   
 
     return (  
     <main>
         <div className="h-100 w-100 border-10 p-10 border-red-200 bg-white">
             <ul>
                 {button.map(b =>(
-                    <button key={b.id} className={'px-4 py-2 ${b.color}'} >{b.name}</button>
+                    <button 
+                    key={b.id} 
+                    className=" m-5 rounded 10 bg-purple-400 
+                                    hover:bg-purple-600 hover:mx-0 hover:px-10 
+                                    py-3 px-5 transtition duration-300">
+                    {b.name}
+                    </button>
                 ))}
             </ul>
         </div>
